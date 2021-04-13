f329eaa843c47f0a42473893b9dfce81b58a9eb8
jmanuellh <manuellh2008@gmail.com>
2021-04-12 19:13:34 -0500

    lista funcionalidad en front

--- Front/palindromos-nuxt/pages/index.vue
+++ Front/palindromos-nuxt/pages/index.vue
@@ -1,34 +1,56 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        palindromos-nuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="container flex flex-col">
+    <div class="text-2xl">
+      Introduzca la cadena de caracteres:
+    </div>
+    <div class="flex flex-row">
+      <input type="text" v-model="cadena"
+      @keyup.enter="enviarCadena()"
+      class="text-xl px-2 py-2 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+      >
+      <button
+        @click="enviarCadena()"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1"
+      >
+        Comprobar
+      </button>
+    </div>
+    <div class="text-2xl">
+      Palíndromos contenidos:
+      <div>
+        <ul v-for="item in palindromos.palindromosContenidos" :key="item">
+          {{item}}
+        </ul>
+      </div>
+    </div>
+    <div class="text-2xl">
+      Palíndromos totales:
+      <div>
+        {{palindromos.palindromosTotales}}
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      cadena: "",
+      palindromos: {
+        palindromosContenidos: [],
+        palindromosTotales: 0
+      }
+    }
+  },
+  methods: {
+    enviarCadena() {
+      this.$axios.get("https://localhost:5001/Palindromos/"+this.cadena).then(response => {
+        this.palindromos = response.data;
+      })
+    }
+  }
+}
 </script>
 
 <style>
